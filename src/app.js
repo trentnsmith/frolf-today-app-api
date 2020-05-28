976d6e8b056ab8f2add89c95d5754b3765174666
Trent Smith <trentnsmith@gmail.com>
2020-05-27 22:47:17 -0500

    requiring validateBearerToken

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+//const validateBearerToken = require('../validateBearerToken')
 
 const app = express()
 
@@ -15,6 +16,7 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+//app.use(validateBearerToken)
 
 app.get('/api/', (req, res) => {
     res.send('/api/ is working')
