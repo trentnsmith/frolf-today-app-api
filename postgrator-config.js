c615ee03b0ac39847cf298812bab3a472e061379
Trent Smith <trentnsmith@gmail.com>
2020-05-30 15:48:23 -0500

    working on migrate

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-    "migrationsDirectory": "migrations",
+    "migrationDirectory": "migrations",
     "driver": "pg",
     "connectionString": process.env.DATABASE_URL,
 }
