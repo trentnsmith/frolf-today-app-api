17589d58eb003972098b3e94da3ed5cd3d353423
Trent Smith <trentnsmith@gmail.com>
2020-06-17 23:52:18 -0500

    removing commented code

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-const validateBearerToken = require('../validateBearerToken')
 const coursesRouter = require('./courses/courses-router')
 
 
@@ -19,7 +18,7 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
-//app.use(validateBearerToken)
+
 
 
 
