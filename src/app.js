741071a167f90e3151d12b539ffec4353b38539e
Trent Smith <trentnsmith@gmail.com>
2020-06-28 19:06:42 -0500

    debugging

--- src/app.js
+++ src/app.js
@@ -29,11 +29,12 @@ app.get('/', (req, res) => {
 //creating server errorHandler
 app.use(function errorHandler(error, req, res, next) {
     let response
+    console.error(error)
+
     if (NODE_ENV === 'production') {
         response = { error: { message: 'server error' } }
     } else {
-        console.error(error)
-        response = { message: error.message, error }
+        response = { message: error.toString(), error }
     }
     res.status(500).json(response)
 });
