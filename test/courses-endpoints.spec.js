8fb24713b81a64b168705f098af290a103cb86ce
Trent Smith <trentnsmith@gmail.com>
2020-06-28 19:07:16 -0500

    updating endpoint tests

--- test/courses-endpoints.spec.js
+++ test/courses-endpoints.spec.js
@@ -1,6 +1,7 @@
-const{ expect } = require('chai')
-const knex = require('knex')
-const app = require('../src/app')
+const knex = require('knex');
+const app = require('../src/app');
+const makeCoursesArray = require('./courses.fixtures');
+const supertest = require('supertest');
 
 describe('Courses endpoints', () => {
     let db
@@ -8,61 +9,45 @@ describe('Courses endpoints', () => {
     before('make knex instance', () => {
         db = knex({
             client: 'pg',
-            connection: process.env.TEST_DB_URL,
+            connection: process.env.TEST_DATABASE_URL,
         })
         app.set('db', db)
     })
 
     after('disconnect from db', () => db.destroy())
 
-    context('Given there are courses in the database', () => {
-        const testCourses = [
-            {
-                courseId: 1, 
-                course_name: "seymour smith", 
-                rating: "5", 
-                holes: 18, 
-                zip: 68102, 
-                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pulvinar ac nisl in bibendum. Vivamus vitae tincidunt ex. Nunc lacinia leo vel dolor ornare laoreet.",
-                location: [41.193860, -96.021848]
-            },
-            {
-                courseId: 2,
-                course_name: "plattsmouth", 
-                rating: "2", 
-                holes: 9, 
-                zip: 68048, 
-                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pulvinar ac nisl in bibendum. Vivamus vitae tincidunt ex. Nunc lacinia leo vel dolor ornare laoreet.",
-                location: [41.006574, -95.898543]
-            },
-            {
-                courseId: 3, 
-                course_name: "omaha course", 
-                rating: "4", 
-                holes: 18, 
-                zip: 68106, 
-                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pulvinar ac nisl in bibendum. Vivamus vitae tincidunt ex. Nunc lacinia leo vel dolor ornare laoreet.",
-                location: [41.339253, -96.046986]
-            },
-            {
-                courseId: 4, 
-                course_name: "lincoln course", 
-                rating: "5", 
-                holes: 18, 
-                zip: 68105, 
-                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pulvinar ac nisl in bibendum. Vivamus vitae tincidunt ex. Nunc lacinia leo vel dolor ornare laoreet.",
-                location: [40.766155, -96.680029]
-            },
-        ];
+    before('cleanup', () => db('frolf_today_courses_test').truncate())
 
-        describe('GET /api/courses', () => {
-            context(`Given no courses`, () => {
-              it(`responds with 200 and an empty list`, () => {
+    afterEach('cleanup', () => db('frolf_today_courses_test').truncate())
+
+    describe('GET /api/courses', () => {
+        context(`Given no courses`, () => {
+          it(`responds with 200 and an empty list`, () => {
+            return supertest(app)
+              .get('/api/courses')
+              .expect(200)
+          })
+        })
+
+        context('Given there are courses in the database', () => {
+            const testCourses = makeCoursesArray();
+
+            beforeEach('insert courses', () => {
+                return db
+                    .into('frolf_today_courses_test')
+                    .insert(testCourses)
+                    .then(() => {
+                        return db
+                        .into('frolf_today_courses_test')
+                        .insert(testCourses)
+                    })
+            })
+
+            it('responds with 200 and all of the courses', () => {
                 return supertest(app)
-                  .get('/api/courses')
-                  .expect(200)
-              })
+                    .get('/api/courses')
+                    .expect(200, testCourses)
+            }) 
         })
     })
-})
 })
